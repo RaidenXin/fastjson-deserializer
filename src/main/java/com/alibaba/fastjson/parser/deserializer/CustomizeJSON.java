9235df00a67ca73425dc89986f838755e8081638
Raiden <549485630@qq.com>
2020-05-04 15:41:58 +0800

    自定义FastJson反序列化处理器

--- src/main/java/com/alibaba/fastjson/parser/deserializer/CustomizeJSON.java
+++ src/main/java/com/alibaba/fastjson/parser/deserializer/CustomizeJSON.java
@@ -2,7 +2,6 @@ package com.alibaba.fastjson.parser.deserializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.parser.ParserConfig;
 
 import java.lang.reflect.Type;
 
